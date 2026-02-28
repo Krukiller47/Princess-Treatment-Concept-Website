fb0fa0a0d8b9fc14b632154771af4a8692444738
Krukiller47 <kirubelalayu.47@gmail.com>
2026-02-28 01:58:39 +0000

    Created Services.js component for services showcase

---
+++ components/Services.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h2>Our Services</h2>
+            <ul>
+                <li>Service 1: Description</li>
+                <li>Service 2: Description</li>
+                <li>Service 3: Description</li>
+                <li>Service 4: Description</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Services;
